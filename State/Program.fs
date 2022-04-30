f220b5251f565c8907dad41ada2b295e370cc514
firest <blankalupo@163.com>
2022-04-30 12:58:10 +0800

    state moand

---
+++ State/Program.fs
@@ -0,0 +1,18 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open State
+
+let test1 x = 
+    state {
+        let! s = get
+        do! put <| x + s
+    }
+
+let test2 x = 
+    state {
+        do! test1 5
+        let! s = get
+        return s + x
+    }
+
+exec (test2 3) 2 |> printfn "State is:%O"
+eval (test2 3) 2 |> printfn "Result is:%O"
---
+++ State/State.fs
@@ -0,0 +1,22 @@
+﻿module State
+
+type Model<'s, 'a> = 's -> 's * 'a
+type State<'s, 'a> = State of Model<'s, 'a>
+
+let run (State m) s = m s
+let eval m s = let (_, a) = run m s in a
+let exec m s = let (s, _) = run m s in s
+
+let get<'s,'a> : State<'s, 's> = State <| fun s -> (s, s)
+let put s = State <| fun _ -> (s, ())
+
+type StateBuilder() =
+    member this.Return(a) = State <| fun s -> (s, a)
+    member this.Bind(m, f) = 
+        State <| fun s ->
+                    let (s', a') = run m s 
+                    run (f a') s'
+
+
+let state<'s, 'a> = new StateBuilder()
+
