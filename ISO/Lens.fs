d02c3ea6241b3496f9125b1c623ba9cde4915d27
firest <blankalupo@163.com>
2022-05-02 19:29:58 +0800

    iso lens

---
+++ ISO/Lens.fs
@@ -0,0 +1,14 @@
+﻿module Lens
+
+type ISO<'a,'b> = ('a -> 'b) * ('b -> 'a)
+type Lens<'a, 'b, 'r> = Lens of ISO<'a, 'b * 'r>
+
+let first f (a, b) =
+    (f a, b)
+
+let private mkConst a b = a
+
+let get (Lens iso) = fst iso >> fst
+let modify (Lens iso) f = fst iso >> first f >> snd iso
+let put l a = modify l (mkConst a)
+
---
+++ ISO/Program.fs
@@ -0,0 +1,26 @@
+﻿open Lens
+
+type Skill = {
+    Damage : int
+    }
+
+type Monster = {
+    Name : string
+    Skill : Skill
+    }
+
+let fwm m = (m.Skill, fun s -> {m with Skill = s})
+let fws s = (s.Damage, fun d -> {s with Damage = d})
+let bw (l, r) = r l 
+
+let com f1 f2 s =
+    let (s2, r1) = f1 s
+    let (s3, r2) = f2 s2
+    (s3, r2 >> r1)
+
+let lens = Lens (com fwm fws, bw)
+
+let monster = {Name = "A"; Skill = {Damage = 12}}
+
+get lens monster |> printfn "Level is:%O"
+put lens 33 monster |> printfn "New Monster is:%O"
